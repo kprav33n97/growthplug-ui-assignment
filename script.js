48bb965de14cb69479f8c045c9e5c5db2658db79
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-08-22 23:40:08 +0530

    Update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-$('.reviews-container').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+const hamburger = document.querySelector('.hamburger');
+const headerTopRight = document.querySelector('.header-top-right');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  headerTopRight.classList.toggle('active');
 });
